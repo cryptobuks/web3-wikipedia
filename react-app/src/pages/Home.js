b40cc37cf183f353265fac94758d38b41b9d4f00
213yuka <pahu.nyax@gmail.com>
2022-11-03 22:24:50 +0900

    on the way home01

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -1,13 +1,41 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [account, setAccount] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [metaMaskFlag, setMetaMaskFlag] = useState(false);
+
+  useEffect(() => {
+    const tmpFlag = window.ethereum && window.ethereum.isMetaMask;
+    setMetaMaskFlag(tmpFlag);
+  }, []);
+
+  const connectWallet = () => {
+    window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then((result) => {
+        setAccount(result[0]);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
+
   return (
-    <>
-      <h1>ホーム</h1>
+    <div className="Home">
+      <h1>Web3 Wikipedia</h1>
+      <button type="button" class="btn btn-secondary">New</button>
+      <div>{account}</div>
       <div>
-        新規登録はこちら
+        {account ? (
+          <button type="button" class="btn btn-dark">Connected</button>
+        ) : (
+          <button type="button" class="btn btn-secondary" onClick={connectWallet}>Connected Wallet</button>
+        )}
       </div>
-    </>
+      <input class="form-control" placeholder="検索"></input>
+    </div>
   );
 };
 
