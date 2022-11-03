a92a9c9a1251c1672e12b435b650f06145ce4750
mayudong200333 <mayudong200333@gmail.com>
2022-11-03 20:05:43 +0900

    change the directory and create the router

---
+++ react-app/src/pages/Home.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <h1>ホーム</h1>
+      <div>
+        新規登録はこちら
+      </div>
+    </>
+  );
+};
+
+export default Home;
